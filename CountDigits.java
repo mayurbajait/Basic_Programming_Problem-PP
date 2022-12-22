8fff05da3d3c17fcc4ea88b502abebef9d6cc2cd
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-22 16:37:31 +0530

    Count Digits
    Write a program to find the total number of digits in a single input number n.
    Your task is to complete the function numDigits which receives n as the parameter and returns the number of digits in n.

---
+++ CountDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+		String num = Integer.toString(n);
+
+		System.out.println(num.length());
+	
+    }
+}
