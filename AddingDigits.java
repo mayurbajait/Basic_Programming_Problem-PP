5f62259a3c3d19d955b6de3c35bd3f2af93fea17
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-23 19:43:58 +0530

    Adding Digits
    Write a program to find the total of digits in a single input number n.

---
+++ AddingDigits.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		int count = 0;
+		while(n>0){
+			int digit = n%10;
+			count = count + digit;
+			n = n/10;
+		}
+		System.out.println(count);
+    }
+}
