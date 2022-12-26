80c520f07df7c93e4697fe14932229e492f3d393
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-26 19:14:43 +0530

    Rectangle Star Pattern
    Printing Rectangle Star Pattern
    In this problem we’re going to code a Java Program for printing rectangle star pattern.
    The process of this program is  take number input from user and store it in variable named as row and col then run the i for loop start from i=1 to i<= row . then take another inner loop start from j=1 to j<=col and then take a star printing statement after that take a line change statement

---
+++ RectangleStarPattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class RectangleStarPattern{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		
+		for(int i = 1; i <= row; i++){
+		    for(int j = 1; j <= col; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
