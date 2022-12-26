9ea209be94ec19be5513c9b702c330196a8d511c
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-26 10:47:53 +0000

    Using While Loop

--- AddingDigits.java
+++ AddingDigits.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class AddingDigits {
     public static void main(String[] args) throws Throwable {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
