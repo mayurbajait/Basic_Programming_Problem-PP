da628dac7b41f1f93d853440299eb0be121bef15
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-27 14:18:15 +0530

    GCD or HCF of two numbers
    Here, in this section we will discuss HCF of two numbers in java.
    GCD(Greatest Common Divisor) or HCF(Highest Common Factor) of two numbers is the number which is the largest common factor of both numbers. It is also referred as Greatest Common Factor(GCF), Greatest Common Measure(GCM), Highest Common Divisor(HCD).

---
+++ GCDorHCFofTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class GCDorHCFofTwoNumbers{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int hcd = 1;
+        for(int i = 1; i <= num1 || i <= num2; i++){
+            if(num1 % i == 0 && num2 % i == 0){
+                hcd = i;
+            }
+        }
+        System.out.println(hcd);
+    }
+}
