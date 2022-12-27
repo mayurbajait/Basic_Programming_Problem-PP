cc7a56688aad94901f9572770af2505f6e2a2d10
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-27 11:52:20 +0530

    Printing Numbered Square Pattern
    In this program we’re going to code a Java Program for printing number square star pattern.
    The logic of this problem is to take a row and column input from user and store in the variable names as row and col and then take a for loop start from int i=1 i<=row and then take inner for loop start from j=1 to j<= col and then print 1 and then change the line

--- NumberedSquarePattern.java
+++ NumberedSquarePattern.java
@@ -1,6 +1,5 @@
 import java.util.*;
-public class Main
-{
+public class NumberedSquarePattern{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
