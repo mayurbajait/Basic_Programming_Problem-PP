434d181c5a03eb29f08f85f82117620cf533c87b
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-27 19:57:29 +0530

    Celsius to Fahrenheit
    Given the temperature of a city in celsius, your task is to convert the temperature to Fahrenheit and return it.
    Note: It is guaranteed that the temperature in celsius, C, will be a multiple of 5.

--- CelsiustoFahrenheit.java
+++ CelsiustoFahrenheit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class CelsiustoFahrenheit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
