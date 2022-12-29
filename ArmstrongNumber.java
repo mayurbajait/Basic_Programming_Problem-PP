c8eb50cc782fc80a2399f7d04e426f777027f816
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-29 16:04:29 +0530

    Number is Armstrong Number or not
    Given an integer input the objective is to check whether or not the number is an Armstrong number. To do so we check if the number satisfies the below mentioned conditions and if it does, the it’s an Armstrong. The task is to write a code to Check Whether or Not the Number is an Armstrong Number.

---
+++ ArmstrongNumber.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+
+class AnyArmstrongNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(), reverse = 0;
+        int len = order(num);
+
+        if (num == getArmstrongSum(num, len))
+            System.out.println(num + " is an Armstrong Number");
+        else
+            System.out.println(num + " is not an Armstrong Number");
+
+    }
+
+    private static int getArmstrongSum(int num, int order) {
+        if(num == 0)
+            return 0;
+
+        int digit = num % 10;
+
+        return (int) Math.pow(digit, order) + getArmstrongSum(num/10, order);
+    }
+
+    private static int order(int num) {
+        int len = 0;
+        while (num!=0)
+        {
+            len++;
+            num = num/10;
+        }
+        return len;
+    }
+
+}
