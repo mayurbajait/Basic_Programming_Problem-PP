62a21d1a24675b78817dce2cdb4660e5d2cf6f35
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2022-12-30 20:30:52 +0530

    Update FactorsofNumber.java

--- FactorsofNumber.java
+++ FactorsofNumber.java
@@ -1,10 +1,10 @@
 import java.util.*;  
-public class Main{  
+public class FactorsofNumber{  
     public static void main(String args[]){  
         Scanner sc = new Scanner(System.in);  
          
         int n = sc.nextInt();  
-        for(int i = 1; i<n; i++){
+        for(int i = 1; i<=n; i++){
             if(n%i==0){
                 System.out.print(i + " ");
             }
