a04214aafd3f55889df31ab85dd939c719a8ab7d
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-02 22:19:45 +0530

    Update IncrementingNumberPattern.java

--- IncrementingNumberPattern.java
+++ IncrementingNumberPattern.java
@@ -4,7 +4,6 @@ public class IncrementingNumberPattern{
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
 		int col = sc.nextInt();
-		
 		for(int i = 1; i <= row; i++){
 		    for(int j = 1; j <= col; j++){
 		        System.out.print(i);
