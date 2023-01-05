f6006684aa402eae429926a76b8039372c1257e2
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-05 13:22:46 +0530

    Create CelsiustoFahrenheit2.java

---
+++ CelsiustoFahrenheit2.java
@@ -0,0 +1,24 @@
+/*
+Celsius to Fahrenheit 2
+Given a temperature in Celsius, output the fahrenheit equivalent of it.
+
+Note: Print complete answer up to 6 decimal places.
+
+Input Format
+Input consists of 1 line of a decimal number.
+
+Output Format
+Output consists of a single line of temperature in Fahrenheit.
+*/
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();
+		double a = (1.8*C)+32;
+		System.out.println(String.format("%.6f", a));
+    
+    }
+}
