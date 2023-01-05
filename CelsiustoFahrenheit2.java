2449f6ef69ae0d5ba20071db527a3badac9d28e9
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-05 13:23:47 +0530

    Update CelsiustoFahrenheit2.java

--- CelsiustoFahrenheit2.java
+++ CelsiustoFahrenheit2.java
@@ -13,7 +13,7 @@ Output consists of a single line of temperature in Fahrenheit.
 
 import java.util.*;
 
-public class Main {
+public class CelsiustoFahrenheit2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int C = sc.nextInt();
