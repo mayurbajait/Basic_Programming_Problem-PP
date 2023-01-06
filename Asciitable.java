6eb334d25ec2e91821bc4a5cbb741f96323edf76
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-06 22:23:48 +0530

    Create Asciitable.java

---
+++ Asciitable.java
@@ -0,0 +1,9 @@
+//To print all ASCII Table charecters,
+
+public class Asciitable {
+    public static void main(String args[]) {
+      for(int i = 0; i<=127; i++){
+            System.out.println((char)i);
+        }
+    }
+}
