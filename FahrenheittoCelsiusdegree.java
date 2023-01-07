45ae4af8be5e93e06d19abd877ffd30e44fc69e9
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-07 15:43:37 +0530

    Update FahrenheittoCelsiusdegree.java

--- FahrenheittoCelsiusdegree.java
+++ FahrenheittoCelsiusdegree.java
@@ -20,8 +20,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Main
-{
+public class FahrenheittoCelsiusdegree{
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
