a9190521799215517c294c3c17887dcdac5f9fa9
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-10 22:36:59 +0530

    Create PrintEvenNo.java

---
+++ PrintEvenNo.java
@@ -0,0 +1,27 @@
+//for loop will runs n/2 times
+import java.util.*;
+public class PrintEvenNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+		for(long i = 0; i <= n; i = i + 2){
+		    System.out.println(count);
+
+		}
+    }
+}
+
+//for loop will runs n times
+import java.util.*;
+public class PrintEvenNo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+		for(long i = 1; i <= n; i++){
+			if(i % 2 == 0){
+				System.out.println(count);
+			}
+		}
+
+    }
+}
