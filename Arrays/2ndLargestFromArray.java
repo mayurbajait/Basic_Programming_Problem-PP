fbdee2f06981656c77bd9c2a4778d2b0aec88a1d
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-18 16:25:42 +0530

    Create 2ndLargestFromArray.java

---
+++ Arrays/2ndLargestFromArray.java
@@ -0,0 +1,64 @@
+/*
+
+2nd Largest from array
+Given an unsorted array of size N with distinct elements. Find the 2nd largest element from the array without sorting the array.
+
+Input Format
+The first line contains a single integer N.
+
+The second line consists of N integers of the array.
+
+Output Format
+Print the second largest number in the new line.
+
+Example 1
+Input
+
+6
+3 2 1 5 6 4
+Output
+
+5
+Explanation
+
+5 is the second largest element of the array.
+
+
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void SecondLargest(int[] arr, int n) {
+		int fmx = arr[0];
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] > fmx){
+				fmx = arr[i];
+			}
+		}
+		int smx = Integer.MIN_VALUE;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] > smx && arr[i] < fmx){
+				smx = arr[i];
+			}
+		}
+		System.out.println(smx);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+            
+        SecondLargest(arr,n);
+        sc.close();
+        
+    }
+}
