01f5e371a46af0b5faac69fa8592a07b19047e2f
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-26 14:52:24 +0530

    Create Sum of Array.java

---
+++ Arrays/Sum of Array.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int findSum(int arr[],int N) {
+        int sum = 0;
+
+		for(int i = 0; i < N; i++){
+			sum = sum + arr[i];
+		}
+		return sum;
+
+    }
+    public static void main(String args[])throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // array size
+        int arr[]=new int[N] ;// creating array of size N
+
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt(); // taking input of array elements
+        }
+
+        int ans=findSum(arr,N);
+        System.out.println(ans); // printing the sum
+    }
+}
